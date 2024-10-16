9446fa6e554e047cc8bab1cef797e73f89254c16
Matkip <Matkip0@users.noreply.github.com>
2024-10-16 09:48:52 +0200

    Added Employee class

---
+++ CheckInSystem/CheckInSystem/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace CheckInSystem.Models;
+
+using Dapper;
+using Database;
+using System.Data.SqlClient;
+
+public class Employee
+{
+    public int ID { get; private set; }
+    public string CardID { get; private set; }
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string LastName { get; set; }
+    public bool IsOffSite { get; set; }
+    public DateTime OffSiteUntil { get; set; }
+
+    
+}
