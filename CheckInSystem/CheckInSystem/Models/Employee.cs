0ceefdc45629a3abe8fb5dbad429df5918ab45f3
Matkip <Matkip0@users.noreply.github.com>
2024-10-24 08:06:15 +0200

    Add GetFromCardId() and INotifyPropertyChanged to Employee

--- CheckInSystem/CheckInSystem/Models/Employee.cs
+++ CheckInSystem/CheckInSystem/Models/Employee.cs
@@ -1,20 +1,61 @@
-﻿namespace CheckInSystem.Models;
+﻿using System.ComponentModel;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace CheckInSystem.Models;
 
 using Dapper;
 using Database;
 using System.Data.SqlClient;
 
-public class Employee
+public class Employee : INotifyPropertyChanged
 {
     public int ID { get; private set; }
-    public string CardID { get; private set; }
-    public string? FirstName { get; set; }
-    public string? MiddleName { get; set; }
-    public string? LastName { get; set; }
+    private string _cardID;
+    public string CardID
+    {
+        get => _cardID;
+        set => SetProperty(ref _cardID, value);
+    }
+    
+    private string? _firstName;
+    public string? FirstName
+    {
+        get => _firstName;
+        set => SetProperty(ref _firstName, value);
+    }
+
+    private string? _middleName;
+    public string? MiddleName
+    {
+        get => _middleName;
+        set => SetProperty(ref _middleName, value);
+    }
+    
+    private string? _lastName;
+
+    public string? LastName
+    {
+        get => _lastName;
+        set => SetProperty(ref _lastName, value);
+    }
+    
     public bool IsOffSite { get; set; }
     public DateTime? OffSiteUntil { get; set; }
-    public bool IsCheckedIn { get; set; }
 
+    private bool _isCheckedIn;
+    public bool IsCheckedIn
+    {
+        get => _isCheckedIn;
+        set => SetProperty(ref _isCheckedIn, value);
+    }
+
+    
+    public void CardScanned()
+    {
+        IsCheckedIn = !IsCheckedIn;
+    }
+    
     public static List<Employee> GetAllEmployees()
     {
         string selectQuery = @"SELECT 
@@ -30,6 +71,26 @@ public class Employee
         using (var connection = new SqlConnection(Database.ConnectionString))
         {
             var employees = connection.Query<Employee>(selectQuery).ToList();
+            return employees;
+        }
+    }
+
+    public static Employee? GetFromCardId(string cardID)
+    {
+        string selectQuery = @"SELECT 
+            id, 
+            cardid, 
+            firstname, 
+            middlename, 
+            lastname, 
+            isoffsite, 
+            offsiteuntil, 
+            [dbo].[IsEmployeeCheckedIn](ID) as IsCheckedIn
+            FROM employee
+            WHERE cardID = @cardID";
+        using (var connection = new SqlConnection(Database.ConnectionString))
+        {
+            var employees = connection.Query<Employee>(selectQuery, new {cardID = cardID}).FirstOrDefault();
             return employees;
         }
     }
@@ -59,6 +120,25 @@ public class Employee
         using (var connection = new SqlConnection(Database.ConnectionString))
         {
             connection.Query(deletionQuery, this);
+        }
+    }
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    protected void SetProperty<T>(ref T variable, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (!EqualityComparer<T>.Default.Equals(variable, value))
+        {
+            variable = value;
+            OnPropertyChanged(propertyName);
         }
     }
 }
