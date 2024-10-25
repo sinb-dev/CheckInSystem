cf9d4f9f0042e363662637ad6c526f8f7e3a8c3a
Matkip <Matkip0@users.noreply.github.com>
2024-10-25 08:19:53 +0200

    Added Converters class

---
+++ CheckInSystem/CheckInSystem/Converters.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Data;
+using LambdaConverters;
+
+namespace CheckInSystem;
+
+public static class Converters
+{
+    public static readonly IValueConverter VisibleIfTrue =
+        ValueConverter.Create<bool, Visibility>(e => e.Value ? Visibility.Visible : Visibility.Collapsed);
+    
+    public static readonly IValueConverter VisibleIfFalse =
+        ValueConverter.Create<bool, Visibility>(e => e.Value ? Visibility.Collapsed : Visibility.Visible);
+}
