ee735ff2a77c3a74804b2a8d3784a9481ee7a269
Matkip <Matkip0@users.noreply.github.com>
2024-10-29 14:14:00 +0100

    Added closing the entire application when main windows is closed

--- CheckInSystem/CheckInSystem/Views/Windows/MainWindow.xaml.cs
+++ CheckInSystem/CheckInSystem/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 using CheckInSystem.ViewModels;
 using CheckInSystem.Views.UserControls;
 
@@ -12,8 +13,13 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        Closing += OnWindowClosing;
         ViewmodelBase.MainContentControl = MainContent;
         MainContent.Content = new LoginScreen();
     }
-
+    
+    public void OnWindowClosing(object sender, CancelEventArgs e)
+    {
+        System.Windows.Application.Current.Shutdown();
+    }
 }
